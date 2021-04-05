48c1960643508c21c65a9430f5f3e36cde8a20cd
aymenkasP <ikoaymen056@gmail.com>
2021-04-05 19:47:46 +0100

    fix header & side bar css

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,45 +3,64 @@ import { useHistory } from 'react-router'
 import '../css/header.css'
 import HamburgerMenu from '../css/menu.png'
 import ShoppingCard from '../css/ShopingCard.svg'
-import {selectCard ,selectSideBar ,ShowSideBzr} from '../features/storeSlice'
-import { useSelector ,useDispatch } from 'react-redux';
+import { selectCard, selectSideBar, ShowSideBzr } from '../features/storeSlice'
+import { useSelector, useDispatch } from 'react-redux';
 import SideBar from './SideBar'
 
 export default function Header() {
     const CardItems = useSelector(selectCard);
     const sideBar = useSelector(selectSideBar)
     const dispatch = useDispatch()
-  const history = useHistory()
-  
-  function open(){
-   history.push("/card")
-  }
+    const history = useHistory()
+
+
+
+    /* open the cart list function */
+    function openCartList() {
+        history.push("/card")
+    }
+
+
     return (
-        <div className ="header">
-        <div className="hdr_left" onClick={()=> {
-            dispatch(ShowSideBzr())
-        }} >
-        <img  src={HamburgerMenu} alt='hamburger' />
-        
-        </div>
 
-        <div className="hdr_center" >
+        
+
+        <div className="header">
+
+            {/* nav bar  */}
+            <div className='nav__bar'>
+                { sideBar && <SideBar /> }
+            </div>
+
+
+            {/* hamburger */}
+            <div className="hdr_left" onClick={() => { dispatch(ShowSideBzr()) }} >
+              {
+                !sideBar? <img src={HamburgerMenu} alt='hamburger' /> : <div></div>
+                
+              }  
+            </div>
+
+
+            {/* the shope name */}
+            <div className="hdr_center" >
                 <p>shopy</p>
-        </div>
+            </div>
 
-        <div className ="hdr_right" onClick={() => {
-           open()
-        }} >
-            <img src={ShoppingCard}  alt="card_shopping" />
-            <span>
-                {CardItems.length === 0 ? '' : CardItems.length}
-            </span>
-        </div>
-       {
-        sideBar && <SideBar /> 
-       }
-        
-        
+
+            {/*  shopping cart */}
+            <div className="hdr_right" onClick={() => { openCartList() }} >
+                <img src={ShoppingCard} alt="card_shopping" />
+                <span>
+                    {CardItems.length === 0 ? '' : CardItems.length}
+                </span>
+            </div>
+
+
+            
+
+
+
         </div>
     )
 }
--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import '../css/sideBar.css'
-import { HideSideBar  } from '../features/storeSlice'
+import { HideSideBar } from '../features/storeSlice'
 export default function SideBar() {
     const dispatch = useDispatch()
-   
+
     console.log(SideBar)
     return (
+
+         /* nav  */
         <div className="sd_bar">
-            <p>electronics</p>
-            <p>jewelery</p>
-            <p>men clothing</p>
-            <p>women clothing</p>
-         
-            <span className ="qut" onClick ={()=> {
-                dispatch(HideSideBar())
-            }} >x</span>
-           
-            
+
+            {/* nav items */} 
+            <div className="sd__item" >
+                <p>electronics</p>
+                <p>jewelery</p>
+                <p>men clothing</p>
+                <p>women clothing</p>
+            </div>
+
+            {/* close the side bar icon */}
+            <div className="sd__qut__icon" >
+               
+                <span className="qut" onClick={() => { dispatch(HideSideBar()) }} >x</span>
+            </div>
+
+
+
         </div>
     )
 }
